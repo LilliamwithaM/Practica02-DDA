29a2a30502e60cae938d4bc8735bce664dc8f91b
Patidifusa <zs20006765@estudiantes.uv.mx>
2023-04-24 01:48:50 -0500

    inicialize

---
+++ src/main/java/org/uv/proyecto02/Proyecto02.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package org.uv.proyecto02;
+
+/**
+ *
+ * @author LilyE
+ */
+public class Proyecto02 {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        singleton.imprimir();
+    }
+}
